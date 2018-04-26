850d54a6e87b3f14e00b2fe65537adaa4f8edc97
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2018-04-27 00:25:57 +0200

    Plataforma
    Plataforma para testear el impulso del player, para hacer las otras restantes se puede hacer una clase base y heredar

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	public float JumpForce = 1f;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (!collision.collider.CompareTag("Player")) return;
+		if (collision.relativeVelocity.y > 0f || collision.transform.position.y < transform.position.y) return;
+
+		Rigidbody2D rigidBody = collision.collider.GetComponent<Rigidbody2D>();
+		Vector2 velocity = rigidBody.velocity;
+		velocity.y = JumpForce;
+		rigidBody.velocity = velocity;
+	}
+}
