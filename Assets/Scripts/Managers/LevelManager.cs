5d6e4206e0f8dc83ef075ff3c4e9cd8027de3113
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2018-04-30 23:35:59 +0200

    Level Manager
    Primera version del generador de nivel, faltan pulir detalles entre distancias de plataformas o enemigos y tiene algun fallo el salto del personaje

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using Patrones;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Managers
+{
+	public class LevelManager : Singleton<LevelManager>
+	{
+		public GameObject Player;
+
+		[Header("Level Properties")]
+		public float BoundUpper = 5.2f;
+		public float LevelWidth = 2.27f;
+		public float LevelHeight = 4.9f;
+
+		[Header("Platforms")]
+		public GameObject NormalPlatform;
+		public GameObject[] SpecialPlatforms;
+		[Header("Enemies")]
+		public GameObject[] Enemies;
+
+		[Header("Probabilities"), Range(0, 1)]
+		public float EnemyProbability;
+		[Range(0, 1)]
+		public float SpecialPlatformProbability;
+
+
+		private int _minDistance;
+		private int _maxDistance = 2;
+		private float _riseTime;
+		private GameObject _finalObject;
+		private List<GameObject> _levelObjects;
+
+		private void Start()
+		{
+			Player = GameObject.FindGameObjectWithTag("Player");
+			_levelObjects = new List<GameObject>();
+			PoolManager.Instance.CreatePool(NormalPlatform, 20);
+			foreach (GameObject platform in SpecialPlatforms)
+			{
+				PoolManager.Instance.CreatePool(platform, 5);
+			}
+			foreach (GameObject enemy in Enemies)
+			{
+				PoolManager.Instance.CreatePool(enemy, 1);
+			}
+
+			const float fraction = 0.06666667F;
+			for (int i = 0; i < 15; i++)
+			{
+				GameObject platform = InstantiateLevelObject(NormalPlatform);
+				Vector3 spawnPosition = platform.transform.position;
+				spawnPosition.y = Mathf.Lerp(-LevelHeight, LevelHeight, fraction * i);
+				platform.transform.position = spawnPosition;
+				if (i == 14)
+				{
+					_finalObject = platform;
+				}
+			}
+		}
+
+		
+
+		private void FixedUpdate()
+		{
+			if (Player.transform.position.y > 0)
+			{
+				Rigidbody2D playerRb = Player.GetComponent<Rigidbody2D>();
+				_riseTime = Time.time + Math.Abs(playerRb.velocity.y / (Physics2D.gravity.y * playerRb.gravityScale));
+				Vector3 newPosition = Player.transform.position;
+				newPosition.y = 0;
+				Player.transform.position = newPosition;
+				playerRb.velocity = Vector2.zero;
+				playerRb.gravityScale = 0;
+			}
+			
+			if (Time.time < _riseTime)
+			{
+				DownLevel();
+			}
+			else
+			{
+				Player.GetComponent<Rigidbody2D>().gravityScale = 1.646f;
+			}
+		}
+
+		private object _lockObject = new object();
+
+		public void DownLevel()
+		{
+			lock (_lockObject)
+			{
+				//Debug.Log($"LevelObjects: {_levelObjects.Count}");
+				for (int i = 0; i < _levelObjects.Count; i++)
+				{
+					GameObject levelObject = _levelObjects[i];
+					Vector3 newPosition = levelObject.transform.position;
+					newPosition.y -= 0.1f;
+					levelObject.transform.position = newPosition;
+					if (newPosition.y <= -BoundUpper - 1)
+					{
+						RemoveLevelObject(levelObject);
+					}
+
+					if (levelObject.transform.position.y > _finalObject.transform.position.y)
+					{
+						_finalObject = levelObject;
+					}
+				}
+				Debug.Log(_finalObject.transform.position.y - (BoundUpper));
+				if (_finalObject.transform.position.y <= (BoundUpper - _maxDistance))
+				{
+					GameObject newLevelObject;
+					if (Random.value <= EnemyProbability)
+					{
+						newLevelObject = Enemies[Random.Range(0, Enemies.Length)];
+					}
+					else
+					{
+						newLevelObject = Random.value < SpecialPlatformProbability ? SpecialPlatforms[Random.Range(0, SpecialPlatforms.Length)] : NormalPlatform;
+					}
+					InstantiateLevelObject(newLevelObject);
+				}
+				GameManager.Instance.Score += 1;
+			}
+		}
+
+		private GameObject InstantiateLevelObject(GameObject levelObject)
+		{
+			lock (_lockObject)
+			{
+				Vector3 spawnPosition = new Vector3
+				{
+					x = Random.Range(-LevelWidth, LevelWidth),
+					y = 5
+				};
+				GameObject finalObject = PoolManager.Instance.Spawn(levelObject, spawnPosition, Quaternion.identity);
+				_levelObjects.Add(finalObject);
+				//_finalObject = finalObject;
+				Debug.Log("Objeto instanciado");
+				return finalObject;
+			}
+			
+		}
+
+		private void RemoveLevelObject(GameObject levelObject)
+		{
+			lock (_lockObject)
+			{
+				int init = _levelObjects.Count;
+				_levelObjects.Remove(levelObject);
+				int final = _levelObjects.Count;
+				PoolManager.Instance.Despawn(levelObject);
+				Debug.Log("Objeto Eliminado");
+			}
+		}
+	}
+}
